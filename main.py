affd6478f11a079cfe563465fcb2dc5a166e0376
Remlas <remlas.bond@gmail.com>
2018-10-20 18:45:41 +0200

    parsing requests json to dictonary function, checking region and locale
    - added file with content of Rift Explorer for quick CTRL+F of hotwords
    - checking for player region and locale - if wrong - changing it.
    - parsing json replies to dictonary

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-##pip install colorama requests wmi pywin32
+##pip3 install colorama requests wmi pywin32
 
 import urllib3
 urllib3.disable_warnings()
 
 import requests
+import json
 import sys
 
 from colorama import init as coloramainit
@@ -24,20 +25,33 @@ headers = {
     'Accept' : 'application/json'
 }
 
-def rqget(url, headers):
+def rqget(url):
     url='https://127.0.0.1:'+LauncherInfo.Credentials.Port+url
     return requests.get(url, headers=headers, auth=('riot', LauncherInfo.Credentials.Pass), verify=False)
-def rqpost(url, headers):
+def rqpost(url):
     url='https://127.0.0.1:'+LauncherInfo.Credentials.Port+url
     return requests.post(url, headers=headers, auth=('riot', LauncherInfo.Credentials.Pass), verify=False)
 
+def jsonget(url, key):
+    return json.loads(rqget(url).text)[key]
+
+##Check Region Settings
+region = jsonget('/riotclient/region-locale','region')
+locale = jsonget('/riotclient/region-locale','locale')
+
+if (region != 'EUNE') or (locale != 'en_GB'):
+    print(Fore.YELLOW + "Wrong Region or Locale. Trying to change it to EUNE/English" + Fore.RESET)
+    rqpost('/riotclient/set_region_locale?region=EUNE&locale=en_GB')
+else:
+    print('Region and Language: ['+ Fore.GREEN + 'OK' + Fore.RESET + ']')
+
 def CheckLauncherScale():
-    LauncherScale = rqget('/riotclient/zoom-scale', headers)
+    LauncherScale = rqget('/riotclient/zoom-scale')
     LauncherScale.json
     if LauncherScale.text == '1.25':
         return Fore.GREEN + 'OK'
     else:
-        rqpost('/riotclient/zoom-scale?newZoomScale=1.25', headers)
+        rqpost('/riotclient/zoom-scale?newZoomScale=1.25')
         return Fore.YELLOW + 'Fixed'
         
 
