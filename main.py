414c93e7f91fffbcf748b46864ca31aaecc9a7ff
Remlas <remlas.bond@gmail.com>
2018-10-26 15:09:23 +0200

    small fixes, changed jsonget to jsongetkey
    value "None" was not working as key

--- main.py
+++ main.py
@@ -35,7 +35,10 @@ def rqpatch(url, data = None):
     url='https://127.0.0.1:'+LauncherInfo.Credentials.Port+url
     return requests.patch(url, headers=headers, auth=('riot', LauncherInfo.Credentials.Pass), data=data, verify=False)
 
-def jsonget(url, key = None):
+def jsonget(url):
+    return json.loads(rqget(url).text)
+
+def jsongetkey(url, key = None):
     return json.loads(rqget(url).text)[key]
 
 def ChampionTileURL(ChampID, skinID = 0):
@@ -50,8 +53,8 @@ def banchamps(greatInt):
             print("dupa")
 
 ##Check Region Settings
-region = jsonget('/riotclient/region-locale','region')
-locale = jsonget('/riotclient/region-locale','locale')
+region = jsongetkey('/riotclient/region-locale','region')
+locale = jsongetkey('/riotclient/region-locale','locale')
 
 if (region != 'EUNE') or (locale != 'en_GB'):
     print(Fore.YELLOW + "Wrong Region or Locale. Trying to change it to EUNE/English" + Fore.RESET)
@@ -72,14 +75,14 @@ if (rqget('/lol-login/v1/session').status_code) == 404:
     import LoginCredentials
     rqpost('/lol-login/v1/session', json.dumps(LoginCredentials.data))
 
-if (jsonget('/lol-login/v1/session','username')) != 'zsltv':
+if (jsongetkey('/lol-login/v1/session','username')) != 'zsltv':
     print("Account: [" + Fore.RED + "ERROR" + Fore.RESET + "]")
     print(Fore.RED + Style.BRIGHT + "You are not using accout dedicated for streaming!\n" + Style.NORMAL + "You have been warned!!!" + Style.RESET_ALL)
     rqpost('/player-notifications/v1/notifications', json.dumps({"critical": "true", "dismissible": "true", "iconUrl": "https://cdn4.iconfinder.com/data/icons/ninja-emoji/512/ninja-17-512.png", "state": "unread", "titleKey": "", "type": ""}))
     rqpost('/lol-simple-dialog-messages/v1/messages', json.dumps({"msgBody": ["You are not logged in on dedicated stream account!"],"msgType": "Wrong accout"}))
 else:
     print("Account:             [" + Fore.GREEN + "OK" + Fore.RESET + "]")
-summonerId = jsonget('/lol-login/v1/session', 'summonerId')
+summonerId = jsongetkey('/lol-login/v1/session', 'summonerId')
 
 print('Launcher Size:       ['+ CheckLauncherScale() + Fore.RESET + ']')
 
@@ -102,11 +105,11 @@ ChampID = 30
 AllChamps = rqget("/lol-champions/v1/inventories/" + str(summonerId) + "/champions").json()
 
 #open('obs/1.jpg', 'wb').write(rqget(ChampionTileURL(ChampID)).content)
-
-ChampID = showchamp()['actions'][countChamps][0]['championId']
-open('obs/1.jpg', 'wb').write(rqget(ChampionTileURL(ChampID)).content)
-"""
 countChamps = 0
+ChampID = showchamp()['actions'][countChamps][0]['championId']
+open('obs/Champ1.jpg', 'wb').write(rqget(ChampionTileURL(ChampID)).content)
+"""
+
 while True:
     banchamps(countChamps)
     if showchamp()['actions'][countChamps][0]['completed'] == True:
